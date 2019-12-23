0e4f79b3a2c965b7c724013c54f82c56e2ff637d
Pujit Mehrotra <pujitm2009@gmail.com>
2019-12-23 21:50:55 +0000

    Add PLAID_PUBLIC_KEY

--- src/config/plaid.js
+++ src/config/plaid.js
@@ -3,7 +3,7 @@ import plaid from 'plaid';
 const plaidClient = new plaid.Client(
   process.env.PLAID_CLIENT_ID,
   process.env.PLAID_SECRET,
-  process.env.PUBLIC_KEY,
+  process.env.PLAID_PUBLIC_KEY,
   plaid.environments[process.env.PLAID_ENV],
   {version: '2018-05-22'}
 );
