8d1acf4695c49bd829d8d1aa283994049fb5a853
Pujit Mehrotra <pujitm2009@gmail.com>
2019-12-23 23:45:51 +0000

    Add missing return statement to create customer

--- src/api/stripe/customer.js
+++ src/api/stripe/customer.js
@@ -1,7 +1,7 @@
 import stripe from "config/stripe";
 
 export async function createCustomer({email, name, source, description, metadata, phone }) {
-  stripe.customers.create({
+  return stripe.customers.create({
     email,
     name,
     source,
--- src/config/stripe.js
+++ src/config/stripe.js
@@ -1,5 +1,5 @@
-import Stripe from 'stripe';
+import * as Stripe from 'stripe';
 
-const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default stripe;
